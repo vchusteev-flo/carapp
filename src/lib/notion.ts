d93f0e26ae3d2beca85ef5cbf82681acea6f9c2b
Vladimir Chusteev <vchusteev@flo.team>
2024-12-02 19:09:51 +0700

    fix: update url notion database service

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -30,7 +30,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3002',
+  baseURL: 'https://cars.flo.team/notion',
   headers: {
     'Content-Type': 'application/json'
   }
