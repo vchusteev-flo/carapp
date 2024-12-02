89de5c97daba09e6cb558b79210d1a85967f267d
Vladimir Chusteev <vchusteev@flo.team>
2024-12-03 05:08:42 +0700

    fix:

--- src/app/api/carInquiry/[inquiryId]/route.js
+++ src/app/api/carInquiry/[inquiryId]/route.js
@@ -44,8 +44,9 @@ export async function GET(request, { params }) {
   });
 
   try {
+    
     console.log(params.id, 'params.id');
-    console.log(params)
+    console.log(await params)
     const inquiry = await getCarInquiryById(params.id);
     return new Response(JSON.stringify(inquiry), {
       status: 200,
