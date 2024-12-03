59c0b96ea7d26598ecfbacd40821501ca8fea090
Vladimir Chusteev <vchusteev@flo.team>
2024-12-03 19:48:55 +0700

    fix: button logic

--- src/components/inquiry-form.tsx
+++ src/components/inquiry-form.tsx
@@ -1,90 +1,99 @@
-'use client'
+'use client';
 
 import { Button } from '@/components/ui/button';
 import { notionClient } from '@/lib/notion';
 import { useEffect, useState } from 'react';
+
 interface CarInquiry {
-	name: string
-	telegramId: number
-	orderCarId: string
-	status: string
-	comments: string
-	price: number
-	finalPrice: string
+  name: string;
+  telegramId: number;
+  orderCarId: string;
+  status: string;
+  comments: string;
+  price: number;
+  finalPrice: string;
 }
 
-export function InquiryForm({
-	id,
-	carPrice,
-}: {
-	id: string
-	carPrice: number
-}) {
-  const [clickFirstly, setclickFirstly] = useState(false);
-	const [carOrdered, setCarOrdered] = useState(false)
-	const [isLoading, setIsLoading] = useState(false)
+export function InquiryForm({ id, carPrice }: { id: string; carPrice: number }) {
+  const [hasButtonBeenClicked, setHasButtonBeenClicked] = useState<boolean>(() => {
+    const storedValue = localStorage.getItem(`hasButtonBeenClicked_${id}`);
+    return storedValue ? JSON.parse(storedValue) : false;
+  });
+  const [carOrdered, setCarOrdered] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-	useEffect(() => {
-		const checkIfCarOrdered = async () => {
-			const telegramUserData = JSON.parse(
-				localStorage.getItem('telegramUser') || '{}'
-			)
-			if (telegramUserData.id) {
-				const inquiries = await notionClient.getCarInquiriesByTelegramId(
-					telegramUserData.id
-				)
-				const hasActiveOrder = inquiries.some(
-					(inquiry: CarInquiry) =>
-						inquiry.orderCarId === id && inquiry.status !== 'Canceled'
-				)
-				setCarOrdered(hasActiveOrder)
-			}
-		}
+  useEffect(() => {
+    // Store hasButtonBeenClicked in localStorage whenever it changes
+    localStorage.setItem(`hasButtonBeenClicked_${id}`, JSON.stringify(hasButtonBeenClicked));
+  }, [hasButtonBeenClicked, id]);
 
-		checkIfCarOrdered()
-	}, [id])
+  useEffect(() => {
+    const checkIfCarOrdered = async () => {
+      const telegramUserData = JSON.parse(localStorage.getItem('telegramUser') || '{}');
+      if (telegramUserData.id) {
+        const inquiries = await notionClient.getCarInquiriesByTelegramId(telegramUserData.id);
+        const hasActiveOrder = inquiries.some(
+          (inquiry: CarInquiry) => inquiry.orderCarId === id && inquiry.status !== 'Canceled'
+        );
+        setCarOrdered(hasActiveOrder);
+      }
+    };
 
-	const handleSubmit = async () => {
-		setIsLoading(true)
-		const telegramUserData = JSON.parse(
-			localStorage.getItem('telegramUser') || '{}'
-		)
+    checkIfCarOrdered();
+  }, [id]);
 
-		const inquiryData = {
-			name: telegramUserData.username,
-			telegramId: telegramUserData.id,
-			orderCarId: id,
-			status: 'New',
-			comments: ' ',
-			price: carPrice,
-			finalPrice: '-',
-		}
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-		const response = await notionClient.createCarInquiry(inquiryData)
-    setTimeout(() => {
+    if (carOrdered) {
+      // Car is already ordered, so we only change the button state locally
+      setHasButtonBeenClicked(true);
+    } else {
+      // Car is not ordered yet, proceed to send the request to the server
+      setIsLoading(true);
+      setHasButtonBeenClicked(true);
+
+      const telegramUserData = JSON.parse(localStorage.getItem('telegramUser') || '{}');
+
+      const inquiryData = {
+        name: telegramUserData.username,
+        telegramId: telegramUserData.id,
+        orderCarId: id,
+        status: 'New',
+        comments: ' ',
+        price: carPrice,
+        finalPrice: '-',
+      };
+
+      const response = await notionClient.createCarInquiry(inquiryData);
+      setIsLoading(false);
       if (response) {
         setCarOrdered(true);
       }
-    }, 1000);
-	}
+    }
+  };
+
   let buttonText;
   if (carOrdered) {
-    buttonText = clickFirstly ? 'Связаться с нами' : 'Запрос на обратную связь отправлен'
+    buttonText = hasButtonBeenClicked
+      ? 'Запрос на обратную связь отправлен'
+      : 'Связаться с нами';
   } else {
-    buttonText = clickFirstly ? 'Сделать расчет': 'Запрос на расчет отправлен';
+    buttonText = hasButtonBeenClicked ? 'Запрос на расчет отправлен' : 'Сделать расчет';
   }
-  
 
-	return (
-    <form action={handleSubmit}>
+  return (
+    <form onSubmit={handleSubmit}>
       <input type="hidden" name="id" value={id} />
-      <Button 
-        type="submit" 
-        className={`px-16 py-8 transition-colors duration-300 ${!isLoading ?'bg-orange-500 text-white': 'bg-green-500 text-black'} font-medium rounded-lg`}
-        onClick={() => setclickFirstly(true)}
+      <Button
+        type="submit"
+        className={`px-16 py-8 transition-colors duration-300 ${
+          hasButtonBeenClicked ? 'bg-green-500 text-black' : 'bg-orange-500 text-white'
+        } font-medium rounded-lg`}
+        disabled={isLoading}
       >
         {buttonText}
       </Button>
     </form>
-	)
+  );
 }
